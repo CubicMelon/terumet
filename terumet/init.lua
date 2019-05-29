b7d174977937a2422aeaaff7c5b7121def477dd6
Scott Horvath <shorvath208@gmail.com>
2019-05-29 12:49:32 -0500

    condensed interop loading

--- terumet/init.lua
+++ terumet/init.lua
@@ -309,18 +309,7 @@ for dmat, v in pairs(dmv_values) do
     terumet.register_repair_material(v[2], v[1])
 end
 
-if minetest.global_exists('armor') then
-    terumet.do_lua_file('interop/3darmor')
-end
-
-if minetest.global_exists('doors') then
-    terumet.do_lua_file('interop/doors')
-end
-
-if minetest.global_exists('unified_inventory') then
-    terumet.do_lua_file('interop/unified_inventory')
-end
-
-if minetest.global_exists('tubelib') then
-    terumet.do_lua_file('interop/tubelib')
+local INTEROPS = {'armor', 'doors', 'unified_inventory', 'tubelib'}
+for _,mod in ipairs(INTEROPS) do
+    if minetest.global_exists(mod) then terumet.do_lua_file('interop/'..mod) end
 end
