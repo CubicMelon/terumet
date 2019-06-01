3266aef639a21676fdb0c6567414a0df6579bdf4
Scott Horvath <shorvath208@gmail.com>
2019-05-31 22:56:29 -0500

    updated split_textures.py

--- split_textures.py
+++ split_textures.py
@@ -1,8 +1,9 @@
 from wand.image import Image
+import os
 
 SOURCE_FILE = 'texsource.png'
 MOD_NAME = 'terumet'
-DEST = 'terumet/textures/'
+DEST = 'split_textures/'
 
 def MT(basename):
     return MOD_NAME + '_' + basename + '.png'
@@ -16,12 +17,15 @@ TEXTURE_FILES = [
     ['block_ceramic', 'ingot_cgls', 'block_cgls', 'tool_pick_cgls', 'tool_shovel_cgls', 'tool_axe_cgls', 'tool_sword_cgls', 'upg_gen_up', 'upg_speed_up', 'upg_tubelib'],
     ['item_entropy', 'item_thermese', 'block_thermese', 'frame_tste', 'frame_cgls', 'frame_raw', 'block_thermese_hot', 'item_htg', 'upg_cryst', 'hline'],
     ['htfurn_front', 'htfurn_top_unlit', 'htfurn_sides', 'htfurn_top_lit', 'vulcan_sides', 'vulcan_top', 'htr_solar_top', 'vulcan_front', 'upg_cheat', 'hline_in'],
-    ['item_heatunit', 'ingot_tcha', 'block_tcha', 'tool_pick_tcha', 'tool_shovel_tcha', 'tool_axe_tcha', 'tool_sword_tcha', 'tool_ore_saw', 'block_entropy', 'item_cryscham'],
-    ['hray_sides', 'hray_front', 'hray_back', 'rayref_sides', 'rayref_front', 'rayref_back', 'tbox_sides', 'tbox_front', 'tbox_back', 'blockov_hline'],
+    ['item_heatunit', 'ingot_tcha', 'block_tcha', 'tool_pick_tcha', 'tool_shovel_tcha', 'tool_axe_tcha', 'tool_sword_tcha', 'tool_ore_saw', 'block_entropy', 'item_cryscham', 'item_rsuitmat'],
+    ['hray_sides', 'hray_front', 'hray_back', 'rayref_sides', 'rayref_front', 'rayref_back', 'tbox_sides', 'tbox_front', 'tbox_back', 'blockov_hline', 'item_rubber'],
     ['tdis_sides', 'tdis_front', 'tdis_back', 'raw_heater_sides', 'tste_heater_sides', 'cgls_heater_sides', 'htr_entropy_top', 'crush_front_unlit', 'crush_front_lit', 'item_tarball', 'vacoven_front'],
     ['item_dust_ob', 'ingot_ttin', 'block_ttin', 'item_press', 'item_dust_wood', 'block_dust_bio', 'item_dust_bio', 'block_pwood', 'block_pwood_sides', 'item_glue', 'vacoven_sides'],
     ['block_tglass_frame', 'block_tglass_streak', 'block_tglassglow_frame', 'block_tglassglow_streak', 'item_rebar', 'blockov_rebar1', 'blockov_rebar2', 'blockov_rebar3', 'item_coke', 'block_coke', 'vacoven_top']
 ]
+
+if not os.path.exists(DEST):
+    os.makedirs(DEST)
 
 with Image(filename=SOURCE_FILE) as source:
     for row in range(len(TEXTURE_FILES)):
